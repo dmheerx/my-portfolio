1e42ecd52ee2ebfd1b13ad0cfbeadec355207e95
Divine Mher Malamdag <divinemherm@gmail.com>
2026-09-25 19:08:57 +0800

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ padding: '20px', backgroundColor: '#333', color: 'white', textAlign: 'center' }}>
+      © 2025 Divine Mher V. Malamdag. All Rights Reserved.
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Hello! I'm Divine Mher V. Malamdag</h1>
+      <p>Welcome to my portfolio website.</p>
+      <div style={{ marginTop: '20px' }}>
+        <img src="/profile.png" alt="Profile" style={{ width: '200px' }} />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
